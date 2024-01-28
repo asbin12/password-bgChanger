725469bd88db14f996f8b34a670e44d31e455ca4
asbin12 <140784814+asbin12@users.noreply.github.com>
2024-01-28 09:19:35 +0545

    password and bg changer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import BgChanger from "./components/bgChanger/BgChanger"
+import PasswordGenerator from "./components/passwordGenerator/PasswordGenerator"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <BgChanger />
+      {/* <PasswordGenerator/> */}
+    </>
+  )
+}
+
+export default App
